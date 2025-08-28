babf2dda42b4cb190067156ab5f71f5330f4836d
superhond-be <paul.thijs1@icloud.com>
2025-08-28 10:16:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 import classesRoutes from "./routes/classes.js";
 import sessionsRoutes from "./routes/sessions.js";
+
+app.use("/api/classes", classesRoutes);
+app.use("/api/sessions", sessionsRoutes);
+
+import classesRoutes from "./routes/classes.js";
+import sessionsRoutes from "./routes/sessions.js";
 app.use("/api/classes", classesRoutes);
 app.use("/api/sessions", sessionsRoutes);
 // server/routes/settings.js
