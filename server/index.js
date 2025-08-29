9a6c7f0a4aba8c9c7c757e92090d02a2a14b27da
superhond-be <paul.thijs1@icloud.com>
2025-08-29 19:17:35 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import settingsRoutes from "./routes/settings.js";
 import customersRoutes from "./routes/customers.js";
 import dogsRoutes from "./routes/dogs.js";
 
+
+
 const app = express();
 app.use(express.json());
 
@@ -16,6 +18,7 @@ app.use("/api/customers", customersRoutes);
 app.use("/api/dogs", dogsRoutes);
 
 // Healthcheck
+
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
 
 // Start server (Render neemt poort uit env var)
