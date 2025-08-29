51c908e0f8c8b0da405cd6adcf473eb098604c1e
superhond-be <paul.thijs1@icloud.com>
2025-08-29 20:24:02 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 import express from "express";
+
 import classesRoutes from "./routes/classes.js";
 import sessionsRoutes from "./routes/sessions.js";
 import settingsRoutes from "./routes/settings.js";
+
 import customersRoutes, { CUSTOMERS } from "./routes/customers.js";
 import dogsRoutes, { setCustomersRef } from "./routes/dogs.js";
 
 const app = express();
 app.use(express.json());
 
-// --- bestaande routes ---
+// bestaande routes
 app.use("/api/classes", classesRoutes);
 app.use("/api/sessions", sessionsRoutes);
 app.use("/api/settings", settingsRoutes);
 
-// --- nieuwe routes ---
+// nieuwe routes
 app.use("/api/customers", customersRoutes);
 app.use("/api/dogs", dogsRoutes);
 
-// geef de klanten-array door aan dogs.js zodat koppeling hond <-> klant werkt
+// geef de klanten-array door aan dogs.js voor koppelen hond ↔ klant
 setCustomersRef(CUSTOMERS);
 
 // healthcheck
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
 
-export default app; // nodig voor ESM (Render start via package.json)
+export default app;
