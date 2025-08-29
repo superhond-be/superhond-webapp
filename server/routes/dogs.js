747a732a74d0312e0f2783bf340b567429e28966
superhond-be <paul.thijs1@icloud.com>
2025-08-29 18:39:22 +0200

    Update dogs.js

--- server/routes/dogs.js
+++ server/routes/dogs.js
@@ -3,14 +3,16 @@ import { CUSTOMERS_REF } from "./customers.js";
 
 const router = express.Router();
 
-// alle honden (over alle klanten)
+// alle honden (uit alle klanten)
 router.get("/", (_req, res) => {
   const CUSTOMERS = CUSTOMERS_REF.get();
-  const dogs = CUSTOMERS.flatMap(c => (c.dogs ?? []).map(d => ({ ...d, ownerId: c.id })));
+  const dogs = CUSTOMERS.flatMap(c =>
+    (c.dogs ?? []).map(d => ({ ...d, ownerId: c.id }))
+  );
   res.json(dogs);
 });
 
-// hond toevoegen aan klant
+// hond toevoegen aan een klant
 router.post("/:customerId", (req, res) => {
   const CUSTOMERS = CUSTOMERS_REF.get();
   const customerId = Number(req.params.customerId);
@@ -23,6 +25,7 @@ router.post("/:customerId", (req, res) => {
   const newDog = { id: (customer.dogs?.at(-1)?.id ?? 0) + 1, name, breed, age };
   customer.dogs = customer.dogs ?? [];
   customer.dogs.push(newDog);
+
   res.status(201).json(newDog);
 });
 
