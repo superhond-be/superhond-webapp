47f548b802ac981b45cb38660e822a85456c0591
superhond-be <paul.thijs1@icloud.com>
2025-08-30 21:35:16 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// server/index.js  (relevant deel)
+// server/index.js
 import express from "express";
 import customersRoutes from "./routes/customers.js";
 import dogsRoutes from "./routes/dogs.js";
@@ -6,11 +6,12 @@ import dogsRoutes from "./routes/dogs.js";
 const app = express();
 app.use(express.json());
 
+// routes koppelen
 app.use("/api/customers", customersRoutes);
 app.use("/api/dogs", dogsRoutes);
 
 // healthcheck
-app.get("/", (_req, res) => res.send("Superhond backend draait!"));
+app.get("/", (_req, res) => res.send("✅ Superhond backend draait!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
