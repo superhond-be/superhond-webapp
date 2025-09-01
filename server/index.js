56cc3452b350026dbe932fb4c66e915e2b9cbf42
superhond-be <paul.thijs1@icloud.com>
2025-09-01 12:27:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+// bovenaan bij de imports
+import searchRoutes from "./routes/search.js";
+
+
 
 // ⤵️ Optioneel: alleen gebruiken als je een routes/debug.js hebt
 import debugRoutes from "./routes/debug.js";
@@ -13,6 +17,9 @@ const __dirname = path.dirname(__filename);
 // App
 const app = express();
 app.use(express.json());
+
+// ... na app en middleware:
+app.use("/api/search", searchRoutes);
 
 // Static frontend (public/)
 app.use(express.static(path.join(__dirname, "..", "public")));
