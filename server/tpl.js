5cb2b33aea3659e79f0bde6ab195ced7fafaf7a9
superhond-be <paul.thijs1@icloud.com>
2025-09-04 10:28:10 +0200

    Update tpl.js

--- server/tpl.js
+++ server/tpl.js
@@ -1,19 +1,7 @@
-// server/tpl.js
-const fs = require('fs');
-const path = require('path');
-
-// simpele placeholder vervanger {{veld}}
-function renderFile(file, data){
-  let out = fs.readFileSync(path.join(__dirname, 'templates', file), 'utf8');
-  for (const [k,v] of Object.entries(data||{})){
-    const re = new RegExp(`{{\\s*${k}\\s*}}`,'g');
-    out = out.replace(re, v==null?'':String(v));
-  }
+const fs = require('fs'); const path = require('path');
+function render(file, data={}){
+  let out = fs.readFileSync(path.join(__dirname,'templates',file),'utf8');
+  for(const [k,v] of Object.entries(data)){ out = out.replace(new RegExp(`{{\\s*${k}\\s*}}`,'g'), v==null?'':String(v)); }
   return out;
 }
-
-function render(file, data){
-  return renderFile(file, data);
-}
-
 module.exports = { render };
