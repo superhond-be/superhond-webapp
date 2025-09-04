e6922169313d52a7acb22f311a6be44798684719
superhond-be <paul.thijs1@icloud.com>
2025-09-04 18:09:24 +0200

    Create sessions.js

---
+++ server/helpers/sessions.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const FILE = path.join(__dirname, '..', 'data', 'sessions.json');
+
+function read() {
+  if (!fs.existsSync(FILE)) return [];
+  return JSON.parse(fs.readFileSync(FILE, 'utf8'));
+}
+function write(arr) {
+  fs.mkdirSync(path.dirname(FILE), { recursive: true });
+  fs.writeFileSync(FILE, JSON.stringify(arr, null, 2));
+}
+const uid = (p='sess') => `${p}-${Date.now()}`;
+
+module.exports = { read, write, uid };
