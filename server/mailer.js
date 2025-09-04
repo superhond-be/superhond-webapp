5f2a950ab4978d16373149a0c18010f8e1928b14
superhond-be <paul.thijs1@icloud.com>
2025-09-04 11:15:30 +0200

    Update mailer.js

--- server/mailer.js
+++ server/mailer.js
@@ -1,6 +1,8 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, port: Number(process.env.SMTP_PORT||587), secure: false,
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT || 587),
+  secure: false,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS }
 });
 async function sendMail({ to, subject, text, html, attachments }) {
