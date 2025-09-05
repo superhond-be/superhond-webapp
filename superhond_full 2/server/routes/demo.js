399470d90dd70e6588e318f4800caa805dc3e99e
superhond-be <paul.thijs1@icloud.com>
2025-09-05 21:14:46 +0200

    Update demo.js

--- superhond_full 2/server/routes/demo.js
+++ superhond_full 2/server/routes/demo.js
@@ -1,5 +1,1 @@
-// demo route
-const express = require('express');
-const router = express.Router();
-router.get('/demo', (req,res)=>res.json({ok:true, msg:'demo werkt'}));
-module.exports = router;
+
