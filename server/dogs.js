1abeda135f4417a561bdd4afcc7647753b627c00
superhond-be <paul.thijs1@icloud.com>
2025-09-05 15:08:18 +0200

    Update dogs.js

--- server/dogs.js
+++ server/dogs.js
@@ -1,22 +1,5 @@
-// server/dogs.js
-
-const { readJSON } = require('./helpers');
-
-const FILE = 'dogs.json';
-
-// Vind hond op ID
-function findDog(id) {
-  const list = readJSON(FILE, []);
-  return list.find(d => d.id === id) || null;
-}
-
-// Vind honden van een bepaalde klant
-function findDogsByClient(client_id) {
-  const list = readJSON(FILE, []);
-  return list.filter(d => d.client_id === client_id);
-}
-
-module.exports = {
-  findDog,
-  findDogsByClient
-};
+const { load, save, path } = require('./fsdb');
+const FILE = path.join(__dirname,'..','data','dogs.json');
+function read(){ return load(FILE) || []; }
+function write(a){ save(FILE, a); }
+module.exports = { read, write };
