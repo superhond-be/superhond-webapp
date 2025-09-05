1201655a63bec64245b38f9842d2406c1e96a573
superhond-be <paul.thijs1@icloud.com>
2025-09-05 21:13:49 +0200

    Update index.js

--- superhond_full 2/server/index.js
+++ superhond_full 2/server/index.js
@@ -1,6 +1,1 @@
-// Superhond main server
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 10000;
-app.get('/', (req,res)=>res.send('Superhond draait!'));
-app.listen(PORT, ()=>console.log(`Superhond server gestart op ${PORT}`));
+
