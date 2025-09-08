ac4b177fbedd39ad08f8ac17a51824121ae78927
superhond-be <paul.thijs1@icloud.com>
2025-09-08 08:28:35 +0200

    Delete public/js directory

--- public/js/admin-login.js
+++
@@ -1,1 +0,0 @@
-console.log("Admin login script geladen");
--- public/js/admin-users.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const list = document.getElementById("list");
-  list.textContent = "Voorbeeld gebruiker: Sofie (superadmin)";
-});
