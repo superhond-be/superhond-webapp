d987100468f2f63f6e83e75e839461d97a275f8e
superhond-be <paul.thijs1@icloud.com>
2025-09-14 18:04:05 +0200

    Update migrate_v29.sql

--- migrate_v29.sql
+++ migrate_v29.sql
@@ -1,5 +1,9 @@
 -- migrate_v29.sql
 -- Zet credits van honden over naar klanten (user_id)
+
 INSERT INTO credit_ledger (user_id, delta, reason)
-SELECT user_id, SUM(credits), 'migration_v29' FROM dogs GROUP BY user_id;
-UPDATE dogs SET credits=0;
+SELECT user_id, SUM(credits), 'migration_v29'
+FROM dogs
+GROUP BY user_id;
+
+UPDATE dogs SET credits = 0;
