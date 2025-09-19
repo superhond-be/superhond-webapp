1e81bfd18dd560bbfa6a661c56596fe35d6ccde1
superhond-be <paul.thijs1@icloud.com>
2025-09-19 12:21:46 +0200

    Add files via upload

--- public/js/seed.js
+++ public/js/seed.js
@@ -1,29 +1,43 @@
 
 (function(){
-  const FLAG='SH_SEEDED_v0173_DEMO';
+  const FLAG='SH_SEEDED_v0174_DEMO';
   if(localStorage.getItem(FLAG)) return;
   function id(p){return p+Math.random().toString(36).slice(2,10)}
-  // basis klanten/honden/klassen simulatie
+
+  // Klanten met honden
   const K1=id('K'),H1=id('H'),K2=id('K'),H2=id('H'),K3=id('K'),H3=id('H'),K4=id('K'),H4=id('H');
   const customers=[
-    {id:K1,voornaam:'Test1',achternaam:'Demo',honden:[{id:H1,naam:'Dog1',credits:2, geldigheid:5}]},
-    {id:K2,voornaam:'Test2',achternaam:'Demo',honden:[{id:H2,naam:'Dog2',credits:2, geldigheid:5}]},
-    {id:K3,voornaam:'Test3',achternaam:'Demo',honden:[{id:H3,naam:'Dog3',credits:2, geldigheid:5}]},
-    {id:K4,voornaam:'Test4',achternaam:'Demo',honden:[{id:H4,naam:'Dog4',credits:2, geldigheid:5}]}
+    {id:K1,voornaam:'Jan',achternaam:'Janssens',honden:[{id:H1,naam:'Bello',credits:5, geldigheid:5}]},
+    {id:K2,voornaam:'An',achternaam:'De Smet',honden:[{id:H2,naam:'Luna',credits:3, geldigheid:3}]},
+    {id:K3,voornaam:'Pieter',achternaam:'Peeters',honden:[{id:H3,naam:'Max',credits:2, geldigheid:6}]},
+    {id:K4,voornaam:'Tom',achternaam:'Vermeer',honden:[{id:H4,naam:'Rex',credits:1, geldigheid:2}]}
   ];
   localStorage.setItem('SH_CUSTOMERS',JSON.stringify(customers));
 
-  const Cwait=id('C');
-  const waitClass={id:Cwait,naam:'Basisgroep vrijdag',type:'Basis',themas:'Volgzaamheid',cap:3,
-    lessen:[{id:id('L'),date:'2025-10-05',time:'18:00',loc:'Dessel',strippen:1},{id:id('L'),date:'2025-10-12',time:'18:00',loc:'Dessel',strippen:1}]};
-  waitClass.startAuto=waitClass.lessen[0].date;
-  localStorage.setItem('SH_CLASSES',JSON.stringify([waitClass]));
+  // Klassen (incl. extra volzet met wachtlijst)
+  function les(d,t,loc,strip){return {id:id('L'),date:d,time:t,loc:loc,strippen:strip}}
+  const C1=id('C'),Cwait=id('C');
+  const classes=[
+    {id:C1,naam:'Puppypack maandag',type:'Puppy',themas:'Socialisatie',cap:8,lessen:[
+      les('2025-10-01','18:00','Retie',1),
+      les('2025-10-08','18:00','Retie',1)
+    ]},
+    {id:Cwait,naam:'Basisgroep vrijdag',type:'Basis',themas:'Volgzaamheid',cap:3,lessen:[
+      les('2025-10-05','18:00','Dessel',1),
+      les('2025-10-12','18:00','Dessel',1)
+    ]}
+  ];
+  classes.forEach(c=>c.startAuto=(c.lessen&&c.lessen[0])?c.lessen[0].date:'');
+  localStorage.setItem('SH_CLASSES',JSON.stringify(classes));
 
+  // Inschrijvingen (incl. wachtlijst)
   const enrolls=[
-    {id:id('E'),klantId:K1,hondId:H1,classId:Cwait,start:waitClass.startAuto,validMonths:5,phase:'goedgekeurd'},
-    {id:id('E'),klantId:K2,hondId:H2,classId:Cwait,start:waitClass.startAuto,validMonths:5,phase:'goedgekeurd'},
-    {id:id('E'),klantId:K3,hondId:H3,classId:Cwait,start:waitClass.startAuto,validMonths:5,phase:'goedgekeurd'},
-    {id:id('E'),klantId:K4,hondId:H4,classId:Cwait,start:waitClass.startAuto,validMonths:5,phase:'wachtlijst'}
+    {id:id('E'),klantId:K1,hondId:H1,classId:C1,start:classes[0].startAuto,validMonths:5,phase:'aanvraag'},
+    {id:id('E'),klantId:K2,hondId:H2,classId:C1,start:classes[0].startAuto,validMonths:3,phase:'goedgekeurd'},
+    {id:id('E'),klantId:K3,hondId:H3,classId:Cwait,start:classes[1].startAuto,validMonths:6,phase:'goedgekeurd'},
+    {id:id('E'),klantId:K2,hondId:H2,classId:Cwait,start:classes[1].startAuto,validMonths:3,phase:'goedgekeurd'},
+    {id:id('E'),klantId:K1,hondId:H1,classId:Cwait,start:classes[1].startAuto,validMonths:5,phase:'goedgekeurd'},
+    {id:id('E'),klantId:K4,hondId:H4,classId:Cwait,start:classes[1].startAuto,validMonths:2,phase:'wachtlijst'}
   ];
   localStorage.setItem('SH_ENROLL',JSON.stringify(enrolls));
 
